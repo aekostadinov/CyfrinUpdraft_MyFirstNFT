ce6215bcf5dad06c31f75fb2d667cfdc883ee83c
aekostadinov <aemilov01@gmail.com>
2024-10-01 01:09:32 +0300

    My First NFT

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,27 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.26;
+
+import {Test} from "../lib/forge-std/src/Test.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCoorect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+        //assertEq(expectedName == actualName);
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+}
